badc0b283b707cac4c42c07eaca42c3c50c281d8
TNBDeSport <51657758+TNBDeSport@users.noreply.github.com>
2019-06-11 00:34:09 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,11 @@ const moment = require('moment');
 module.exports = client => {
  console.log(chalk.black.bgRed('Bot en ligne'));
  console.log(chalk.inverse(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] `) + chalk.black.bgGreen(`${client.user.tag} est prêt.`));
-  client.user.setActivity('#TeamNeverBackDown',{type:'LISTENING'})
+  //client.user.setActivity('#TeamNeverBackDown',{type:'LISTENING'})
+
+client.user.setActivity("#TeamNeverBackDown", {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/tnbdesport"
+});
 };
 // on off
